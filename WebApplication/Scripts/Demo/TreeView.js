742ecf8dd41698257e1bd7c42677887a8e03d916
Dicky Suryadi <dickysuryadi@hotmail.com>
2015-12-26 21:18:08 -0800

    Revert "Moved old Javascript examples to Demo.PlainJS folder."
    This reverts commit 1c4813381f6f81731300d370bfcdf3574c143da2.

---
+++ WebApplication/Scripts/Demo/TreeView.js
@@ -0,0 +1,25 @@
+var TreeViewVM = (function () {
+    function TreeViewVM() {
+    }
+    TreeViewVM.prototype.isSelected = function (iItem) { return this.SelectedId() == iItem.Id(); };
+    TreeViewVM.prototype.expandIcon = function (iItem) { return iItem.Expanded() ? 'glyphicon-minus' : 'glyphicon-plus'; };
+    TreeViewVM.prototype.expand = function (iItem, iElement, iParentItem) {
+        var vm = this;
+        if (iItem.Children() == null) {
+            // Setting the item's Expanded property to true will cause the server to load the subitems 
+            // and send the item with its subitems back through ExpandedItem property.
+            iItem.Expanded(true);
+            // When the new tree item in the ExpandedItem is received, use it to replace the one here.
+            vm.$once(vm.ExpandedItem, function (iNewItem) {
+                if (iItem.Id() == iNewItem.Id())
+                    iParentItem.Children.replace(iItem, iNewItem);
+                // The iNewItem object now belongs to the tree, and must be removed from ExpandedItem.
+                vm.ExpandedItem(null);
+            });
+        }
+        else
+            // If the subitems are already here, just toggle the expanded state locally.
+            vm.$preventBinding(function () { return iItem.Expanded(!iItem.Expanded()); });
+    };
+    return TreeViewVM;
+})();
---
+++ WebApplication/Scripts/Demo/demoApp.js
@@ -0,0 +1,31 @@
+﻿require.config({
+   baseUrl: '/Scripts',
+   paths: {
+      "jquery": "jquery-1.11.3.min",
+      "jquery-ui": "jquery-ui-widget-1.11.4.min",
+      "knockout": "knockout-3.3.0",
+      "ko-mapping": "knockout.mapping-latest",
+      "dotnetify": "dotnetify",
+      "router": "dotnetify.router",
+      "path": "path.min",
+      "signalr": "jquery.signalR-2.2.0.min",
+      "signalr-hub": "/signalr/hubs?",
+      "bootstrap": "DemoLibs/bootstrap.min",
+      "calendar": "DemoLibs/zabuto_calendar.min",
+   },
+   shim: {
+      "jquery": { exports: "$" },
+      "knockout": { exports: "ko" },
+      "path": { exports: "Path" },
+      "bootstrap": ["jquery"],
+      "calendar": ["jquery"],
+      "signalr": { deps: ["jquery"], exports: "$.connection" },
+      "signalr-hub": ["signalr"]
+   }
+});
+
+require(['jquery', 'knockout', 'dotnetify', 'router', 'path', 'bootstrap', 'calendar'], function ($) {
+   $(function () {
+      dotnetify.debug = true;
+   });
+});
