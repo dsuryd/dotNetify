8689ee034b59dee196f091eff99290c45ab4acf8
dicky <dicky@DESKTOP-99P3RLO>
2017-03-02 21:07:59 -0800

    Added IDotNetifyConfiguration; refactored startup config.

--- WebApplication/Global.asax.cs
+++ WebApplication/Global.asax.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
-using DotNetify;
-using ViewModels;
 
 namespace WebApplication
 {
@@ -12,34 +9,6 @@ namespace WebApplication
       {
          AreaRegistration.RegisterAllAreas();
          RouteConfig.RegisterRoutes(RouteTable.Routes);
-
-         // Find the DEMO assembly "ViewModels" and register it to DotNetify.VMController.
-         // This will cause all the classes inside the assembly that inherits from DotNetify.BaseVM to be known as view models.
-         var vmAssembly = System.Reflection.Assembly.Load("ViewModels");
-         if (vmAssembly != null)
-            VMController.RegisterAssembly(vmAssembly);
-
-         // The following demonstrates dotNetify's capability to support dependency injection.
-         // In this example, the responsibility of constructing the model object is delegated 
-         // away from its view model.  You can use your favorite IoC container to provide 
-         // view model instances here.
-         VMController.CreateInstance = (type, args) =>
-         {
-            if (type == typeof(SimpleListVM))
-               return new SimpleListVM(new EmployeeModel(7));
-            else if (type == typeof(BetterListVM))
-               return new BetterListVM(new EmployeeModel(7));
-            else if (type == typeof(AFITop100VM))
-               return new AFITop100VM(new AFITop100Model());
-            else if (type == typeof(TreeViewVM))
-               return new TreeViewVM(new EmployeeModel());
-            else if (type == typeof(GridViewVM))
-               return new GridViewVM(new EmployeeModel());
-            else if (type == typeof(CompositeViewVM))
-               return new CompositeViewVM(new EmployeeModel());
-
-            return Activator.CreateInstance(type, args);
-         };
       }
    }
 }
--- WebApplication/OWINStartup.cs
+++ WebApplication/OWINStartup.cs
@@ -1,15 +1,46 @@
-﻿using Microsoft.Owin;
+﻿using System;
+using Microsoft.Owin;
 using Owin;
+using DotNetify;
+using ViewModels;
 
 [assembly: OwinStartup(typeof(WebApplication.OWINStartup))]
 
 namespace WebApplication
 {
-    public class OWINStartup
-    {
-        public void Configuration(IAppBuilder app)
-        {
-            app.MapSignalR();
-        }
-    }
+   public class OWINStartup
+   {
+      public void Configuration(IAppBuilder app)
+      {
+         app.MapSignalR();
+         app.UseDotNetify(config =>
+         {
+            // Register the DEMO assembly "ViewModels". All subclasses of DotNetify.BaseVM 
+            // inside that assembly will be known as view models.
+            config.RegisterAssembly("ViewModels");
+
+            // The following demonstrates dotNetify's capability to support dependency injection.
+            // In this example, the responsibility of constructing the model object is delegated 
+            // away from its view model.  You can use your favorite IoC container to provide 
+            // view model instances here.
+            config.SetFactoryMethod((type, args) =>
+            {
+               if (type == typeof(SimpleListVM))
+                  return new SimpleListVM(new EmployeeModel(7));
+               else if (type == typeof(BetterListVM))
+                  return new BetterListVM(new EmployeeModel(7));
+               else if (type == typeof(AFITop100VM))
+                  return new AFITop100VM(new AFITop100Model());
+               else if (type == typeof(TreeViewVM))
+                  return new TreeViewVM(new EmployeeModel());
+               else if (type == typeof(GridViewVM))
+                  return new GridViewVM(new EmployeeModel());
+               else if (type == typeof(CompositeViewVM))
+                  return new CompositeViewVM(new EmployeeModel());
+
+               return Activator.CreateInstance(type, args);
+            });
+         });
+      }
+   }
 }
