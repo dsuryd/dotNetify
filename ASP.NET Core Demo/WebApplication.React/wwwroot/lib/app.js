95e99002230c1198a19195710324552a99f61716
dicky <dicky@DESKTOP-99P3RLO>
2017-10-14 10:35:53 -0700

    Cleanup.

--- ASP.NET Core Demo/WebApplication.React/wwwroot/lib/app.js
+++ ASP.NET Core Demo/WebApplication.React/wwwroot/lib/app.js
@@ -1,6 +1,5 @@
 ﻿$ = jQuery = require("jquery"); // Add jQuery to global window for Bootstrap.
 require("bootstrap");
-// require("./no-jquery.signalR");
 dotnetifyHub = require("./dotnetify-hub");
 React = require("react");
 ReactDOM = require("react-dom");
