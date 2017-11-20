ba5fb84b133060e99029064ad783641d940f63df
dicky <dicky@DESKTOP-99P3RLO>
2017-11-19 19:11:33 -0800

    Updated unit tests.

---
+++ ASP.NET Core Demo/UnitTests/HelloWorldVMTest.cs
@@ -0,0 +1,76 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using DotNetify;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace UnitTests
+{
+   [TestClass]
+   public class HelloWorldVMTest
+   {
+      private ResponseStub _response = new ResponseStub();
+
+      private class HelloWorldVM : BaseVM
+      {
+         public string FirstName
+         {
+            get => Get<string>() ?? "Hello";
+            set
+            {
+               Set(value);
+               Changed(nameof(FullName));
+            }
+         }
+
+         public string LastName
+         {
+            get => Get<string>() ?? "World";
+            set
+            {
+               Set(value);
+               Changed(nameof(FullName));
+            }
+         }
+
+         public string FullName => $"{FirstName} {LastName}";
+      }
+
+      [TestMethod]
+      public void HelloWorldVM_Request()
+      {
+         VMController.Register<HelloWorldVM>();
+
+         var vmController = new VMController(_response.Handler);
+         vmController.OnRequestVM("conn1", typeof(HelloWorldVM).Name);
+
+         Assert.AreEqual(typeof(HelloWorldVM).Name, _response.VMId);
+         var vm = _response.GetVM<HelloWorldVM>();
+         Assert.IsNotNull(vm);
+         Assert.AreEqual("Hello", vm.FirstName);
+         Assert.AreEqual("World", vm.LastName);
+         Assert.AreEqual("Hello World", vm.FullName);
+      }
+
+      [TestMethod]
+      public void HelloWorldVM_Update()
+      {
+         VMController.Register<HelloWorldVM>();
+
+         var vmController = new VMController(_response.Handler);
+         vmController.OnRequestVM("conn1", typeof(HelloWorldVM).Name);
+
+         var update = new Dictionary<string, object>() { { "FirstName", "John" } };
+         vmController.OnUpdateVM("conn1", typeof(HelloWorldVM).Name, update);
+
+         Assert.IsNotNull(_response.VMData);
+         Assert.AreEqual("John World", _response.VMData["FullName"]);
+
+         update = new Dictionary<string, object>() { { "LastName", "Doe" } };
+         vmController.OnUpdateVM("conn1", typeof(HelloWorldVM).Name, update);
+
+         Assert.IsNotNull(_response.VMData);
+         Assert.AreEqual("John Doe", _response.VMData["FullName"]);
+      }
+   }
+}
---
+++ ASP.NET Core Demo/UnitTests/ResponseStub.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace UnitTests
+{
+   public class ResponseStub
+   {
+      private string _connectionId;
+      private string _vmId;
+      private string _vmData;
+
+      public string VMId => _vmId;
+      public JObject VMData => (JObject)JsonConvert.DeserializeObject(_vmData);
+
+      public void Handler(string connectionId, string vmId, string vmData)
+      {
+         _connectionId = connectionId;
+         _vmId = vmId;
+         _vmData = vmData;
+      }
+
+      public T GetVM<T>() where T : class => JsonConvert.DeserializeObject<T>(_vmData);
+      public T GetVMProperty<T>(string propName) where T : class => VMData[propName].ToObject(typeof(T)) as T; 
+
+      public Dictionary<string, object> MockAction(string actionName, string actionValue) => new Dictionary<string, object>() { { actionName, actionValue } };
+   }
+}
