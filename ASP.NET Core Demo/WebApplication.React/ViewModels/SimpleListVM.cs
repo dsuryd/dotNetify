83f3e939c4b8d1af27200eec3ce53cfb5ecf0eed
dicky <dicky@DESKTOP-99P3RLO>
2017-12-02 10:47:39 -0800

    Refactored to use runtime reactive properties.

--- ASP.NET Core Demo/WebApplication.React/ViewModels/SimpleListVM.cs
+++ ASP.NET Core Demo/WebApplication.React/ViewModels/SimpleListVM.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using DotNetify;
 
 namespace ViewModels
@@ -23,100 +24,74 @@ namespace ViewModels
       }
 
       /// <summary>
-      /// List of employees.
-      /// </summary>
-      public IEnumerable<EmployeeInfo> Employees => _employeeService.GetAll().Select(i => new EmployeeInfo
-      {
-         Id = i.Id,
-         FirstName = i.FirstName,
-         LastName = i.LastName
-      });
-
-      /// <summary>
-      /// If you use CRUD methods on a list, you must set the item key prop name of that list
-      /// by defining a string property that starts with that list's prop name, followed by "_itemKey".
-      /// </summary>
-      public string Employees_itemKey => nameof(EmployeeInfo.Id);
-
-      /// <summary>
-      /// When the Add button is clicked, this property will receive the new employee full name input.
-      /// </summary>
-      public Action<string> Add => fullName =>
-      {
-         var names = fullName.Split(new char[] { ' ' }, 2);
-         var newRecord = new EmployeeModel
-         {
-            FirstName = names.First(),
-            LastName = names.Length > 1 ? names.Last() : ""
-         };
-
-         // Call special base method to add the new employee info back to the list on the client-side.
-         // This will be handled by dotNetify client-side library; no custom JSX needed.
-         this.AddList(nameof(Employees), new EmployeeInfo
-         {
-            Id = _employeeService.Add(newRecord),
-            FirstName = newRecord.FirstName,
-            LastName = newRecord.LastName
-         });
-      };
-
-      /// <summary>
-      /// When a list item is edited, this property will receive the edited item.
-      /// </summary>
-      public Action<EmployeeInfo> Update => changes =>
-      {
-         /// Real world app would do database update operation here.
-         var record = _employeeService.GetById(changes.Id);
-         if (record != null)
-         {
-            record.FirstName = changes.FirstName ?? record.FirstName;
-            record.LastName = changes.LastName ?? record.LastName;
-            _employeeService.Update(record);
-
-            ShowNotification = true;
-         }
-      };
-
-      /// <summary>
-      /// When the Remove button is clicked, this property will receive the employee Id to remove.
-      /// </summary>
-      public Action<int> Remove => id =>
-      {
-         _employeeService.Delete(id);
-
-         // Call special base method to remove an item from the list on the client-side.
-         // This will be handled by dotNetify client-side library; no custom JSX needed.
-         this.RemoveList(nameof(Employees), id);
-      };
-
-      /// <summary>
-      /// Whether to show notification that changes have been saved.
-      /// Once this property is accessed, it will revert itself back to false.
-      /// </summary>
-      private bool _showNotification;
-      public bool ShowNotification
-      {
-         get
-         {
-            var value = _showNotification;
-            _showNotification = false;
-            return value;
-         }
-         set
-         {
-            _showNotification = value;
-            Changed(nameof(ShowNotification));
-         }
-      }
-
-      /// <summary>
       /// Constructor.
       /// </summary>
-      /// <param name="model">Employee model.</param>
       public SimpleListVM()
       {
          // Normally this will be constructor-injected.
          _employeeService = new EmployeeService(7);
+
+         // List of employees.
+         AddReactiveProperty<IEnumerable<EmployeeInfo>>("Employees").SubscribeTo(
+            Observable.Return(_employeeService
+               .GetAll()
+               .Select(i => new EmployeeInfo
+               {
+                  Id = i.Id,
+                  FirstName = i.FirstName,
+                  LastName = i.LastName
+               }))
+         );
+
+         // If you use CRUD methods on a list, you must set the item key prop name of that list
+         // by defining a string property that starts with that list's prop name, followed by "_itemKey".
+         AddReactiveProperty<string>("Employees_itemKey").SubscribeTo(Observable.Return(nameof(EmployeeInfo.Id)));
+
+         // When the Add button is clicked, this property will receive the new employee full name input.
+         this.AddReactiveProperty<string>("Add").Skip(1).Subscribe(fullName =>
+         {
+            var names = fullName.Split(new char[] { ' ' }, 2);
+            var newRecord = new EmployeeModel
+            {
+               FirstName = names.First(),
+               LastName = names.Length > 1 ? names.Last() : ""
+            };
+
+            this.AddList("Employees", new EmployeeInfo
+            {
+               Id = _employeeService.Add(newRecord),
+               FirstName = newRecord.FirstName,
+               LastName = newRecord.LastName
+            });
+         });
+
+         // Property to show notification when changes have been saved.
+         var showNotification = AddReactiveProperty<bool>("ShowNotification");
+
+         // When a list item is edited, this property will receive the edited item.
+         AddReactiveProperty<EmployeeInfo>("Update").Skip(1).Subscribe(changes =>
+         {
+            /// Real world app would do database update operation here.
+            var record = _employeeService.GetById(changes.Id);
+            if (record != null)
+            {
+               record.FirstName = changes.FirstName ?? record.FirstName;
+               record.LastName = changes.LastName ?? record.LastName;
+               _employeeService.Update(record);
+
+               showNotification.OnNext(true);
+            }
+         });
+
+         // When the Remove button is clicked, this property will receive the employee Id to remove.
+         AddReactiveProperty<int>("Remove").Skip(1).Subscribe(id =>
+         {
+            _employeeService.Delete(id);
+
+            // Call special base method to remove an item from the list on the client-side.
+            // This will be handled by dotNetify client-side library; no custom JSX needed.
+            this.RemoveList("Employees", id);
+         });
       }
    }
 }
