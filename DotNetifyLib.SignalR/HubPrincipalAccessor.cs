2e2761dc70bf1ac836e487adff126d6beffd3e07
dicky <dicky@DESKTOP-99P3RLO>
2018-03-31 20:59:05 -0700

    Added copyright verbiage.

--- DotNetifyLib.SignalR/HubPrincipalAccessor.cs
+++ DotNetifyLib.SignalR/HubPrincipalAccessor.cs
@@ -1,9 +1,28 @@
-﻿using System.Security.Principal;
+﻿/*
+Copyright 2016-2018 Dicky Suryadi
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
+using System.Security.Principal;
 using DotNetify.Security;
 using System.Threading;
 
 namespace DotNetify
 {
+   /// <summary>
+   /// Implements ambient data store for the current principal of the hub.
+   /// </summary>
    internal class HubPrincipalAccessor : IPrincipalAccessor
    {
       private readonly static AsyncLocal<IPrincipal> _asyncLocal = new AsyncLocal<IPrincipal>();
