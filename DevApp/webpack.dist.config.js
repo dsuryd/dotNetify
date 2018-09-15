7107df7b52a22f3e11fa871974ba1b14fb3bb4b7
Dicky Suryadi <Dicky.Suryadi@corp.int>
2018-09-14 17:47:01 -0700

    Copy typings to dist.

--- DevApp/webpack.dist.config.js
+++ DevApp/webpack.dist.config.js
@@ -5,100 +5,106 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const baseExport = {
-  module: {
-    rules: [
-      { test: /\.jsx?$/, use: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.tsx?$/, use: 'awesome-typescript-loader?silent=true' },
-      { test: /\.css$/, use: [ MiniCssExtractPlugin.loader, 'css-loader?minimize' ] },
-      { test: /\.svg$/, use: 'svg-url-loader?noquotes=true' },
-      { test: /\.(png|jpg|jpeg|gif)$/, use: 'url-loader?limit=25000' },
-      { test: /\.(eot|ttf|woff(2)?)(\?v=\d+\.\d+\.\d+)?/, loader: 'url-loader' }
-    ]
-  },
-  resolve: {
-    modules: [ 'src', 'node_modules' ],
-    extensions: [ '.js', '.jsx', '.tsx' ]
-  },
-  plugins: [ new MiniCssExtractPlugin(), new CopyWebpackPlugin([ { from: './src/dotnetify/libs/signalR-netfx.js', to: './' } ]) ],
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new UglifyJsPlugin({
-        include: /\.min\.js$/
-      })
-    ]
-  }
+	module: {
+		rules: [
+			{ test: /\.jsx?$/, use: 'babel-loader', exclude: /node_modules/ },
+			{ test: /\.tsx?$/, use: 'awesome-typescript-loader?silent=true' },
+			{ test: /\.css$/, use: [ MiniCssExtractPlugin.loader, 'css-loader?minimize' ] },
+			{ test: /\.svg$/, use: 'svg-url-loader?noquotes=true' },
+			{ test: /\.(png|jpg|jpeg|gif)$/, use: 'url-loader?limit=25000' },
+			{ test: /\.(eot|ttf|woff(2)?)(\?v=\d+\.\d+\.\d+)?/, loader: 'url-loader' }
+		]
+	},
+	resolve: {
+		modules: [ 'src', 'node_modules' ],
+		extensions: [ '.js', '.jsx', '.tsx' ]
+	},
+	plugins: [
+		new MiniCssExtractPlugin(),
+		new CopyWebpackPlugin([
+			{ from: './src/dotnetify/libs/signalR-netfx.js', to: './' },
+			{ from: './src/dotnetify/typings', to: './typings' }
+		])
+	],
+	optimization: {
+		minimize: true,
+		minimizer: [
+			new UglifyJsPlugin({
+				include: /\.min\.js$/
+			})
+		]
+	}
 };
 
 module.exports = [
-  Object.assign(
-    {
-      mode: 'production',
-      entry: {
-        'dotnetify-react': './src/dotnetify/react/index.js',
-        'dotnetify-react.min': './src/dotnetify/react/index.js'
-      },
-      output: {
-        path: __dirname + '/dist/dist',
-        library: 'dotnetify',
-        libraryTarget: 'umd'
-      },
-      externals: {
-        react: {
-          commonjs: 'react',
-          commonjs2: 'react',
-          amd: 'react',
-          root: 'React'
-        },
-        'react-dom': {
-          commonjs: 'react-dom',
-          commonjs2: 'react-dom',
-          amd: 'react-dom',
-          root: 'ReactDOM'
-        },
-        '@aspnet/signalr': {
-          commonjs: '@aspnet/signalr',
-          commonjs2: '@aspnet/signalr',
-          amd: '@aspnet/signalr',
-          root: 'signalR'
-        }
-      }
-    },
-    baseExport
-  ),
-  Object.assign(
-    {
-      mode: 'production',
-      entry: {
-        'dotnetify-ko': './src/dotnetify/knockout/index.js',
-        'dotnetify-ko.min': './src/dotnetify/knockout/index.js'
-      },
-      output: {
-        path: __dirname + '/dist/dist',
-        library: 'dotnetify',
-        libraryTarget: 'umd'
-      },
-      externals: {
-        knockout: {
-          commonjs: 'knockout',
-          commonjs2: 'knockout',
-          amd: 'knockout',
-          root: 'ko'
-        },
-        jquery: {
-          commonjs: 'jquery',
-          commonjs2: 'jquery',
-          amd: 'jquery',
-          root: 'jQuery'
-        },
-        '@aspnet/signalr': {
-          commonjs: '@aspnet/signalr',
-          commonjs2: '@aspnet/signalr',
-          amd: '@aspnet/signalr',
-          root: 'signalR'
-        }
-      }
-    },
-    baseExport
-  )
+	Object.assign(
+		{
+			mode: 'production',
+			entry: {
+				'dotnetify-react': './src/dotnetify/react/index.js',
+				'dotnetify-react.min': './src/dotnetify/react/index.js'
+			},
+			output: {
+				path: __dirname + '/dist/dist',
+				library: 'dotnetify',
+				libraryTarget: 'umd'
+			},
+			externals: {
+				react: {
+					commonjs: 'react',
+					commonjs2: 'react',
+					amd: 'react',
+					root: 'React'
+				},
+				'react-dom': {
+					commonjs: 'react-dom',
+					commonjs2: 'react-dom',
+					amd: 'react-dom',
+					root: 'ReactDOM'
+				},
+				'@aspnet/signalr': {
+					commonjs: '@aspnet/signalr',
+					commonjs2: '@aspnet/signalr',
+					amd: '@aspnet/signalr',
+					root: 'signalR'
+				}
+			}
+		},
+		baseExport
+	),
+	Object.assign(
+		{
+			mode: 'production',
+			entry: {
+				'dotnetify-ko': './src/dotnetify/knockout/index.js',
+				'dotnetify-ko.min': './src/dotnetify/knockout/index.js'
+			},
+			output: {
+				path: __dirname + '/dist/dist',
+				library: 'dotnetify',
+				libraryTarget: 'umd'
+			},
+			externals: {
+				knockout: {
+					commonjs: 'knockout',
+					commonjs2: 'knockout',
+					amd: 'knockout',
+					root: 'ko'
+				},
+				jquery: {
+					commonjs: 'jquery',
+					commonjs2: 'jquery',
+					amd: 'jquery',
+					root: 'jQuery'
+				},
+				'@aspnet/signalr': {
+					commonjs: '@aspnet/signalr',
+					commonjs2: '@aspnet/signalr',
+					amd: '@aspnet/signalr',
+					root: 'signalR'
+				}
+			}
+		},
+		baseExport
+	)
 ];
