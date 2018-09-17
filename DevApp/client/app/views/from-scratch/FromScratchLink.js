548ceedb17eb6f55700520819d700316b03a1484
dicky <dicky@DESKTOP-99P3RLO>
2018-09-16 18:43:11 -0700

    Corrected website links.

--- DevApp/client/app/views/from-scratch/FromScratchLink.js
+++ DevApp/client/app/views/from-scratch/FromScratchLink.js
@@ -3,18 +3,18 @@ import React from 'react';
 const redirect = path => window.dotnetify.react.router.pushState({}, null, path);
 
 const FromScratchLink = _ => (
-	<ul style={{ fontSize: '1.1rem' }}>
-		<li>
-			<a href="javascript:void(0)" onClick={_ => redirect('/fromscratch-scripttag')}>
-				Real-time "Hello World" from scratch with .NET Core CLI + Script Tag
-			</a>
-		</li>
-		<li>
-			<a href="javascript:void(0)" onClick={_ => redirect('/fromscratch-webpack')}>
-				Real-time "Hello World" from scratch with Visual Studio + WebPack
-			</a>
-		</li>
-	</ul>
+  <ul style={{ fontSize: '1.1rem' }}>
+    <li>
+      <a href="javascript:void(0)" onClick={_ => redirect('/core/fromscratch-scripttag')}>
+        Real-time "Hello World" from scratch with .NET Core CLI + Script Tag
+      </a>
+    </li>
+    <li>
+      <a href="javascript:void(0)" onClick={_ => redirect('/core/fromscratch-webpack')}>
+        Real-time "Hello World" from scratch with Visual Studio + WebPack
+      </a>
+    </li>
+  </ul>
 );
 
 export default FromScratchLink;
