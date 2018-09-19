727e2ce0db6eefdeffd1398d25b62a4c4df15840
dicky <dicky@DESKTOP-99P3RLO>
2018-09-18 23:01:41 -0700

    Add IMulticast.

---
+++ UnitTests/MulticastTest.cs
@@ -0,0 +1,24 @@
+﻿using DotNetify;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace UnitTests
+{
+   [TestClass]
+   public class MulticastTest
+   {
+      private class MulticastVM : BaseVM, IMulticast
+      {
+         public List<string> Messages { get; } = new List<string>();
+      }
+
+      [TestMethod]
+      public void MulticastVM_Test()
+      {
+         var vmController = new MockVMController<MulticastVM>();
+         var response = vmController.RequestVM();
+
+         //Assert.AreEqual("Hello", response.GetVMProperty<string>("FirstName"));
+      }
+   }
+}
