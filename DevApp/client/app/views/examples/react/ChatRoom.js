4a7ae3573d80dbd3c0fe0ba4ad8bb4f4a72efab8
dicky <dicky@DESKTOP-99P3RLO>
2018-09-27 21:42:55 -0700

    Update.

--- DevApp/client/app/views/examples/react/ChatRoom.js
+++ DevApp/client/app/views/examples/react/ChatRoom.js
@@ -5,87 +5,101 @@ import TextBox from '../components/TextBox';
 import { ChatRoomCss } from '../components/css';
 
 class ChatRoom extends React.Component {
-	static contextTypes = { connect: PropTypes.func };
+  static contextTypes = { connect: PropTypes.func };
 
-	constructor(props, context) {
-		super(props, context);
-		this.state = { Users: [], Messages: [], message: '' };
+  constructor(props, context) {
+    super(props, context);
+    this.state = { Users: [], Messages: [], message: '' };
 
-		this.vm = dotnetify.react.connect('ChatRoomVM', this);
-		this.dispatchState = state => this.vm.$dispatch(state);
+    this.vm = dotnetify.react.connect('ChatRoomVM', this);
+    this.dispatchState = state => this.vm.$dispatch(state);
 
-		this.scrollToBottom = () => this.bottomElem.scrollIntoView({ behavior: 'smooth' });
-	}
+    this.scrollToBottom = () => this.bottomElem.scrollIntoView({ behavior: 'smooth' });
+  }
 
-	componentDidMount() {
-		this.dispatchState({ AddUser: null });
-		this.scrollToBottom();
-	}
+  componentDidMount() {
+    this.correlationId = `${Math.random()}`;
+    this.dispatchState({ AddUser: this.correlationId });
+    this.scrollToBottom();
+  }
 
-	componentDidUpdate() {
-		this.scrollToBottom();
-	}
+  componentDidUpdate() {
+    this.scrollToBottom();
+    if (this.state.PrivateMessage) {
+      let message = this.state.PrivateMessage;
+      message.Text = '(private) ' + message.Text;
+      message.private = true;
+      this.setState({ Messages: this.state.Messages.concat(message) });
+      this.setState({ PrivateMessage: null });
+    }
+  }
 
-	componentWillUnmount() {
-		this.dispatchState({ RemoveUser: null });
-		this.vm.$destroy();
-	}
+  componentWillUnmount() {
+    this.dispatchState({ RemoveUser: null });
+    this.vm.$destroy();
+  }
 
-	getUserName(userId) {
-		const user = this.state.Users.find(x => x.Id === userId);
-		return user ? user.Name : null;
-	}
+  getUserName(userId) {
+    const user = this.state.Users.find(x => x.Id === userId);
+    return user ? user.Name : null;
+  }
 
-	sendMessage(text) {
-		var match = /name is ([A-z]+)/i.exec(text);
-		this.dispatchState({
-			Send: {
-				Text: text,
-				Date: new Date(),
-				UserName: match ? match[1] : ''
-			}
-		});
-		this.setState({ message: '' });
-	}
+  sendMessage(text) {
+    var match = /name is ([A-z]+)/i.exec(text);
+    this.dispatchState({
+      Send: {
+        Text: text,
+        Date: new Date(),
+        UserName: match ? match[1] : ''
+      }
+    });
+    this.setState({ message: '' });
+  }
 
-	render() {
-		return (
-			<ChatRoomCss>
-				<div className="chatPanel">
-					<nav>
-						{this.state.Users.map(user => (
-							<p key={user.Id}>
-								<b>{user.Name}</b>
-								<span>{user.IpAddress}</span>
-								<span>{user.Browser}</span>
-							</p>
-						))}
-					</nav>
-					<section>
-						<div>
-							{this.state.Messages.map(msg => (
-								<div key={msg.Id}>
-									<div>
-										<span>{this.getUserName(msg.UserId) || msg.UserName}</span>
-										<span>{new Date(msg.Date).toLocaleString()}</span>
-									</div>
-									<div>{msg.Text}</div>
-								</div>
-							))}
-							<div style={{ float: 'left', clear: 'both' }} ref={el => (this.bottomElem = el)} />
-						</div>
-						<TextBox
-							placeholder="Type your message here"
-							value={this.state.message}
-							onChange={value => this.setState({ message: value })}
-							onUpdate={value => this.sendMessage(value)}
-						/>
-					</section>
-				</div>
-				<footer>* Hint: type 'my name is ___' to introduce yourself.</footer>
-			</ChatRoomCss>
-		);
-	}
+  render() {
+    return (
+      <ChatRoomCss>
+        <div className="chatPanel">
+          <nav>
+            {this.state.Users.map(user => (
+              <p key={user.Id}>
+                <b className={user.CorrelationId == this.correlationId ? 'myself' : ''}>{user.Name}</b>
+                <span>{user.IpAddress}</span>
+                <span>{user.Browser}</span>
+              </p>
+            ))}
+          </nav>
+          <section>
+            <div>
+              {this.state.Messages.map((msg, idx) => (
+                <div key={idx}>
+                  <div>
+                    <span>{this.getUserName(msg.UserId) || msg.UserName}</span>
+                    <span>{new Date(msg.Date).toLocaleString()}</span>
+                  </div>
+                  <div className={msg.private ? 'private' : ''}>{msg.Text}</div>
+                </div>
+              ))}
+              <div style={{ float: 'left', clear: 'both' }} ref={el => (this.bottomElem = el)} />
+            </div>
+            <TextBox
+              placeholder="Type your message here"
+              value={this.state.message}
+              onChange={value => this.setState({ message: value })}
+              onUpdate={value => this.sendMessage(value)}
+            />
+          </section>
+        </div>
+        <footer>
+          <div>* Hint:</div>
+          <ul>
+            <li>type 'my name is ___' to introduce yourself</li>
+            <li>type '&lt;username&gt;: ___' to send private message</li>
+          </ul>
+        </footer>
+      </ChatRoomCss>
+    );
+  }
 }
 
 export default ChatRoom;
