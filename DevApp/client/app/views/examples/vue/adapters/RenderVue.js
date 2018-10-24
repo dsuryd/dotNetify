e6a634ee811e8b8b7ce25eee8c12014258cb1e2b
Dicky Suryadi <Dicky.Suryadi@corp.int>
2018-10-24 14:41:23 -0700

    Removed unmount. It's now handled by the library.

--- DevApp/client/app/views/examples/vue/adapters/RenderVue.js
+++ DevApp/client/app/views/examples/vue/adapters/RenderVue.js
@@ -2,15 +2,11 @@ import React from 'react';
 import dotnetify from 'dotnetify';
 
 export default class RenderVue extends React.Component {
-  componentDidMount() {
-    const self = this;
-    dotnetify.vue.router.$mount('.example-root.vue', this.props.src, this.props.htmlAttrs).then(vue => (self.vue = vue));
-  }
-  componentWillUnmount() {
-    this.vue.$destroy();
-  }
+	componentDidMount() {
+		dotnetify.vue.router.$mount('.example-root.vue', this.props.src, this.props.htmlAttrs);
+	}
 
-  render() {
-    return <section className="example-root vue" />;
-  }
+	render() {
+		return <section className="example-root vue" />;
+	}
 }
