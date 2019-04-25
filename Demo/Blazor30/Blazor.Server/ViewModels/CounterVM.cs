84d32ded3fb6ac6063fe6ffc864fdddf0be49a80
dicky <dicky@DESKTOP-99P3RLO>
2019-04-24 22:23:21 -0700

    WIP.

---
+++ Demo/Blazor30/Blazor.Server/ViewModels/CounterVM.cs
@@ -0,0 +1,21 @@
+﻿using DotNetify;
+using System.Windows.Input;
+
+namespace Blazor.Server
+{
+   public class CounterVM : BaseVM
+   {
+      public CounterVM()
+      {
+         CurrentCount = 100;
+      }
+
+      public int CurrentCount
+      {
+         get => Get<int>();
+         set => Set(value);
+      }
+
+      public ICommand IncrementCount => new Command(() => CurrentCount++);
+   }
+}
