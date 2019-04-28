41d4e6cc4f6356a9af3ec27987e65d55c4997243
dicky <dicky@DESKTOP-99P3RLO>
2019-04-27 21:33:39 -0700

    Functioning FetchData with VMContext.

--- Demo/Blazor30/Blazor.Client/Startup.cs
+++ Demo/Blazor30/Blazor.Client/Startup.cs
@@ -1,3 +1,4 @@
+using DotNetify.Blazor;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,6 +8,7 @@ namespace Blazor.Client
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.UseDotNetifyBlazor();
         }
 
         public void Configure(IComponentsApplicationBuilder app)
