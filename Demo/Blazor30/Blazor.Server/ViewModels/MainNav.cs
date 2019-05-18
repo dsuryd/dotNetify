c6eb0cbd360cbf0ddf614e59496766c76ec67b27
Dicky Suryadi <Dicky.Suryadi@corp.int>
2019-05-17 17:53:28 -0700

    Update nav.

---
+++ Demo/Blazor30/Blazor.Server/ViewModels/MainNav.cs
@@ -0,0 +1,24 @@
+﻿using DotNetify;
+using DotNetify.Elements;
+using DotNetify.Routing;
+
+namespace Blazor.Server.ViewModels
+{
+    public class MainNav : BaseVM, IRoutable
+    {
+        public RoutingState RoutingState { get; set; }
+
+        public MainNav()
+        {
+            AddProperty("NavMenu", new NavMenu(
+            new NavMenuItem[]
+            {
+                new NavRoute("Home", this.Redirect("/", "")),
+                new NavRoute("Counter", this.Redirect("/", "counter")),
+                new NavRoute("Fetch", this.Redirect("/", "fetchdata")),
+                new NavRoute("Dashboard", this.Redirect("/", "dashboard")),
+                new NavRoute("Form", this.Redirect("/", "form")),
+            }));
+        }
+    }
+}
