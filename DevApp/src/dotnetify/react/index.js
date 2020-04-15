e61e113e7a38efdf690ff4c5b85e50975af72637
dicky <dicky@DESKTOP-99P3RLO>
2020-04-14 21:04:01 -0700

    File rename.

--- DevApp/src/dotnetify/react/index.js
+++ DevApp/src/dotnetify/react/index.js
@@ -1,6 +1,6 @@
 import dotnetify from './dotnetify-react';
 import './dotnetify-react.router';
-import enableSsr from './ssr';
+import enableSsr from './dotnetify-react.router.ssr';
 import Scope from './Scope';
 import RouteLink from './RouteLink';
 import RouteTarget from './RouteTarget';
