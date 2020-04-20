f1abf88fc8036ce8f2f4b999613c3b5792515214
dicky <dicky@DESKTOP-99P3RLO>
2020-04-19 21:40:25 -0700

    Update dependencies to RC version temporarily.

--- Demo/React/LazyLoadRouting/wwwroot/dist/main.js
+++ Demo/React/LazyLoadRouting/wwwroot/dist/main.js
@@ -93,7 +93,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "92426eaa4971a27474e1";
+/******/ 	var hotCurrentHash = "197839a95391202c71b3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
