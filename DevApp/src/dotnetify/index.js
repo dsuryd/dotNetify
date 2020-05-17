fc38c6e2a122f010b9f0b0da5a57f8d5622fa7aa
dicky <dicky@DESKTOP-99P3RLO>
2020-05-17 14:57:18 -0700

    Remove duplicate line.

--- DevApp/src/dotnetify/index.js
+++ DevApp/src/dotnetify/index.js
@@ -1,9 +1,8 @@
-import dotnetify, { Scope, RouteLink, useConnect } from './react';
+import dotnetify, { Scope, RouteLink, useConnect } from "./react";
 
 export { Scope, RouteLink, useConnect };
 export default dotnetify;
 
 if (window) {
   window.dotNetify = dotnetify;
-  window.dotnetify = dotnetify;
 }
