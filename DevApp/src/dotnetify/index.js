b35d1a343a6294f4380b4cea09d3bfc3d63f88f9
dicky <dicky@DESKTOP-99P3RLO>
2020-05-17 15:26:54 -0700

    Update.

--- DevApp/src/dotnetify/index.js
+++ DevApp/src/dotnetify/index.js
@@ -3,6 +3,4 @@ import dotnetify, { Scope, RouteLink, useConnect } from "./react";
 export { Scope, RouteLink, useConnect };
 export default dotnetify;
 
-if (window) {
-  window.dotNetify = dotnetify;
-}
+if (window) Object.assign(window, { dotnetify, dotNetify: dotnetify });
