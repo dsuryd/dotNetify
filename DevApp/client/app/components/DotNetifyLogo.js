9bf77692ae5002a7142647ebfbff750be12bcbfa
dicky <dicky@DESKTOP-99P3RLO>
2020-06-18 13:57:48 -0700

    Update style.

--- DevApp/client/app/components/DotNetifyLogo.js
+++ DevApp/client/app/components/DotNetifyLogo.js
@@ -15,7 +15,7 @@ export const LicenseNotice = styled.div`
   margin-top: auto;
   margin-left: auto;
   padding: 5px 8px;
-  font-size: .8rem;
+  font-size: 0.8rem;
   color: #999;
   a {
     color: #337ab7;
@@ -78,13 +78,36 @@ const twitterIconCss = `
     }   
 `;
 
+const githubSponsorCss = `
+   border: 1px solid #ccc; 
+   border-radius: 6px; 
+   padding: 3px 12px; 
+   background: #fafbfc;
+   text-align: center;
+   max-width: 8rem;
+   box-shadow: 0 1px 0 rgba(27,31,35,.04), inset 0 1px 0 hsla(0,0%,100%,.25);
+   a { display: flex; align-items: center; justify-content: center; }
+   svg { color: #ea4aaa; fill: currentColor; margin-right: 5px; }
+`;
+
 export const GitHubLink = _ => (
-  <Panel padding="1rem 1rem .5rem 1rem; &:hover { background: #efefef; }">
+  <Panel padding="1rem 1rem .5rem 1rem; &:hover { background: #efefef; }" horizontal middle>
     <a href="https://github.com/dsuryd/dotNetify">
       <Label icon={<GitHubIcon />} css="cursor: pointer">
         GitHub
       </Label>
     </a>
+    <d-panel css={githubSponsorCss}>
+      <a href="https://github.com/sponsors/dsuryd/">
+        <svg height="16" viewBox="0 0 16 16" version="1.1" width="16">
+          <path
+            fillRule="evenodd"
+            d="M4.25 2.5c-1.336 0-2.75 1.164-2.75 3 0 2.15 1.58 4.144 3.365 5.682A20.565 20.565 0 008 13.393a20.561 20.561 0 003.135-2.211C12.92 9.644 14.5 7.65 14.5 5.5c0-1.836-1.414-3-2.75-3-1.373 0-2.609.986-3.029 2.456a.75.75 0 01-1.442 0C6.859 3.486 5.623 2.5 4.25 2.5zM8 14.25l-.345.666-.002-.001-.006-.003-.018-.01a7.643 7.643 0 01-.31-.17 22.075 22.075 0 01-3.434-2.414C2.045 10.731 0 8.35 0 5.5 0 2.836 2.086 1 4.25 1 5.797 1 7.153 1.802 8 3.02 8.847 1.802 10.203 1 11.75 1 13.914 1 16 2.836 16 5.5c0 2.85-2.045 5.231-3.885 6.818a22.08 22.08 0 01-3.744 2.584l-.018.01-.006.003h-.002L8 14.25zm0 0l.345.666a.752.752 0 01-.69 0L8 14.25z"
+          />
+        </svg>{' '}
+        Sponsor
+      </a>
+    </d-panel>
   </Panel>
 );
 
