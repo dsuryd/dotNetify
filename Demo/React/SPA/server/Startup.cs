52da6447b21446b48bc0942f659c9e623ed7e3da
dicky <dicky@DESKTOP-99P3RLO>
2020-08-02 20:13:51 -0700

    Update to .NET Core 3.1.

--- Demo/React/SPA/server/Startup.cs
+++ Demo/React/SPA/server/Startup.cs
@@ -1,11 +1,8 @@
-﻿using System.IO;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using DotNetify;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.DependencyInjection;
-using DotNetify;
 
 namespace SPA
 {
@@ -13,7 +10,6 @@ namespace SPA
    {
       public void ConfigureServices(IServiceCollection services)
       {
-         services.AddMemoryCache();
          services.AddSignalR();
          services.AddDotNetify();
 
@@ -24,20 +20,22 @@ namespace SPA
       public void Configure(IApplicationBuilder app)
       {
          app.UseWebSockets();
-         app.UseSignalR(routes => routes.MapDotNetifyHub());
          app.UseDotNetify();
 
+#pragma warning disable 618
          app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
          {
             HotModuleReplacement = true,
             HotModuleReplacementClientOptions = new Dictionary<string, string> { { "reload", "true" } },
          });
+#pragma warning restore 618
 
          app.UseStaticFiles();
-         app.Run(async (context) =>
+         app.UseRouting();
+         app.UseEndpoints(endpoints =>
          {
-            using (var reader = new StreamReader(File.OpenRead("wwwroot/index.html")))
-               await context.Response.WriteAsync(reader.ReadToEnd());
+            endpoints.MapHub<DotNetifyHub>("/dotnetify");
+            endpoints.MapFallbackToFile("index.html");
          });
       }
    }
