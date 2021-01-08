09709954fb97ed02ac1ab683ed4eb9bccf6ac101
dicky <dicky@DESKTOP-99P3RLO>
2021-01-07 18:22:44 -0800

    Fix typescript typing (#255, #271).

--- DevApp/src/dotnetify/_typings/index.ts
+++ DevApp/src/dotnetify/_typings/index.ts
@@ -6,15 +6,16 @@ export default dotnetify;
 
 export interface IDotnetify {
   // Supported JS framework.
-  react?: IDotnetifyReact;
-  vue?: IDotnetifyVue;
-  ko?: any;
+  react: IDotnetifyReact;
+  vue: IDotnetifyVue;
+  ko: any;
 
   // SignalR hub options.
   hub: IDotnetifyHub;
   hubOptions: HubOptionsType;
   hubPath: string;
   hubLib: any;
+  hubServerUrl: string;
 
   // Debug mode.
   debug: boolean;
@@ -28,7 +29,7 @@ export interface IDotnetify {
   ) => void;
 
   // Use this intercept a view model prior to establishing connection.
-  connectHandler: (args: VMConnectArgsType) => VMConnectArgsType;
+  connectHandler: (args: VMConnectArgsType) => VMConnectArgsType | void;
 
   // Connect to server. Use it for non-supported frameworks.
   connect: (vmId: string, options?: IConnectOptions) => IDotnetifyVM;
@@ -36,8 +37,8 @@ export interface IDotnetify {
   // Creates a SignalR hub client.
   createHub: (
     iHubServerUrl: string,
-    iHubPath: string,
-    iHubLib: any
+    iHubPath?: string,
+    iHubLib?: any
   ) => IDotnetifyHub;
 
   // Creates a Web API hub client.
@@ -115,8 +116,8 @@ export interface IEventEmitter {
 }
 
 export type HubOptionsType = {
-  transport: string[];
-  connectionBuilder: (builder: HubConnectionBuilder) => HubConnectionBuilder;
+  transport?: string[];
+  connectionBuilder?: (builder: HubConnectionBuilder) => HubConnectionBuilder;
 };
 
 export type RequestPayloadType = {
