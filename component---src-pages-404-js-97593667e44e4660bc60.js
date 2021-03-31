f32d091147fa9e9b8aea99c41c8f5377ce5524cf
dicky <dicky@DESKTOP-99P3RLO>
2021-03-30 22:00:06 -0700

    Updates

---
+++ component---src-pages-404-js-97593667e44e4660bc60.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_blog=self.webpackChunkgatsby_starter_blog||[]).push([[883],{9616:function(t,e,a){"use strict";a.r(e);var n=a(7294),l=a(7198),s=a(3751);e.default=function(t){var e=t.data,a=t.location,o=e.site.siteMetadata.title;return n.createElement(l.Z,{location:a,title:o},n.createElement(s.Z,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-97593667e44e4660bc60.js.map
