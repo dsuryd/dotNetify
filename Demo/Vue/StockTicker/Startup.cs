02fab4eccdb63032824f3619b5695031d03bee07
dicky <dicky@DESKTOP-99P3RLO>
2021-04-03 14:31:41 -0700

    Add MapFallbackToFile.

--- Demo/Vue/StockTicker/Startup.cs
+++ Demo/Vue/StockTicker/Startup.cs
@@ -1,8 +1,5 @@
-﻿using System.IO;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.DependencyInjection;
 using DotNetify;
@@ -35,12 +32,10 @@ namespace StockTicker
 
          app.UseStaticFiles();
          app.UseRouting();
-         app.UseEndpoints(endpoints => endpoints.MapHub<DotNetifyHub>("/dotnetify"));
-
-         app.Run(async (context) =>
-         {
-            using (var reader = new StreamReader(File.OpenRead("wwwroot/index.html")))
-               await context.Response.WriteAsync(reader.ReadToEnd());
+         app.UseEndpoints(endpoints =>
+         { 
+            endpoints.MapHub<DotNetifyHub>("/dotnetify"); 
+            endpoints.MapFallbackToFile("index.html");
          });
       }
    }
