a6a2f977503b6fded915a349ff1755610b69bec5
dicky <dicky@DESKTOP-99P3RLO>
2021-07-25 22:35:48 -0700

    Update init-db.sql.

--- Demo/React/RealtimeDb.Postgres/init-db.sql
+++ Demo/React/RealtimeDb.Postgres/init-db.sql
@@ -2,6 +2,9 @@ ALTER SYSTEM SET wal_level='logical';
 ALTER SYSTEM SET max_wal_senders='10';
 ALTER SYSTEM SET max_replication_slots='10';
 
+CREATE PUBLICATION dotnetify_pub FOR ALL TABLES;
+SELECT * FROM pg_create_logical_replication_slot('dotnetify_slot', 'pgoutput');
+
 CREATE USER my_user WITH PASSWORD 'my_pwd';
 ALTER ROLE my_user WITH REPLICATION;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO my_user;
@@ -18,7 +21,4 @@ CREATE TABLE IF NOT EXISTS accounts (
 	balance decimal,
 	created_on TIMESTAMP NOT NULL
 );
-
-CREATE PUBLICATION dotnetify_pub FOR ALL TABLES;
-SELECT * FROM pg_create_logical_replication_slot('dotnetify_slot', 'pgoutput');
 
