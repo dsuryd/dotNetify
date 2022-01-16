d106bd50ccf9e40c6be0d475dac8487af9d30680
dicky <dicky@DESKTOP-99P3RLO>
2022-01-16 13:42:51 -0800

    Merge Startup to Program.

--- DevApp/server/Program.cs
+++ DevApp/server/Program.cs
@@ -1,18 +1,92 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using System.Text;
+using DotInitializr;
+using DotNetify;
+using DotNetify.DevApp;
+using DotNetify.Pulse;
+using DotNetify.Security;
+using Jering.Javascript.NodeJS;
+using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.IdentityModel.Tokens;
 
-namespace DotNetify.DevApp
+var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+
+// Add OpenID Connect server to produce JWT access tokens.
+services.AddAuthenticationServer();
+
+services.AddSignalR()
+   //.AddMessagePackProtocol()
+   ;
+services.AddDotNetify();
+services.AddDotNetifyPulse();
+services.AddDotInitializr();
+services.AddMvc();
+
+services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+services.AddSingleton<IMovieService, MovieService>();
+services.AddSingleton<IWebStoreService, WebStoreService>();
+
+StaticNodeJSService.Configure<OutOfProcessNodeJSServiceOptions>(options => options.TimeoutMS = 2000);
+
+var app = builder.Build();
+
+app.UseAuthentication();
+app.UseWebSockets();
+app.UseDotNetify(config =>
 {
-   public class Program
-   {
-      public static void Main(string[] args)
-      {
-         BuildWebHost(args).Run();
-      }
+   config.RegisterAssembly("DotNetify.DevApp.ViewModels");
 
-      public static IWebHost BuildWebHost(string[] args) =>
-          WebHost.CreateDefaultBuilder(args)
-              .UseStartup<Startup>()
-              .Build();
-   }
+   var tokenValidationParameters = new TokenValidationParameters
+   {
+      IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(AuthServer.SecretKey)),
+      ValidateIssuerSigningKey = true,
+      ValidateAudience = false,
+      ValidateIssuer = false,
+      ValidateLifetime = true,
+      ClockSkew = TimeSpan.FromSeconds(0)
+   };
+
+   // Middleware to do authenticate token in incoming request headers.
+   config.UseJwtBearerAuthentication(tokenValidationParameters);
+
+   // Filter to check whether user has permission to access view models with [Authorize] attribute.
+   config.UseFilter<AuthorizeFilter>();
+
+   // Middleware to log incoming/outgoing message; default to Sytem.Diagnostic.Trace.
+   var logger = app.Services.GetService<ILogger<VMController>>();
+   config.UseDeveloperLogging(log =>
+   {
+      if (!log.Contains("vmId=PulseVM"))
+         logger.LogInformation(log);
+      System.Diagnostics.Trace.WriteLine(log);
+   });
+
+   // Demonstration middleware that extracts auth token from incoming request headers.
+   config.UseMiddleware<ExtractAccessTokenMiddleware>(tokenValidationParameters);
+
+   // Demonstration filter that passes access token from the middleware to the ViewModels.SecurePageVM class instance.
+   config.UseFilter<SetAccessTokenFilter>();
+});
+app.UseDotNetifyPulse();
+
+if (app.Environment.IsDevelopment())
+{
+#pragma warning disable 618
+   app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+   {
+      HotModuleReplacement = true,
+      HotModuleReplacementClientOptions = new Dictionary<string, string> { { "reload", "true" } },
+   });
+#pragma warning restore 618
 }
+
+app.UseStaticFiles();
+app.UseRouting();
+app.MapHub<DotNetifyHub>("/dotnetify");
+app.MapControllers();
+
+//app.UseSsr(typeof(App), (string[] args) => StaticNodeJSService.InvokeFromFileAsync<string>("wwwroot/ssr", null, args), DefaultRequestHandler);
+
+app.MapFallbackToFile("index.html");
+app.Run();
