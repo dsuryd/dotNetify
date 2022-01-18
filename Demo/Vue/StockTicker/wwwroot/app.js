e6c770895fabf273d4c5d8984cd830351bd35d9f
dicky <dicky@DESKTOP-99P3RLO>
2022-01-17 21:54:35 -0800

    Refactor using minimal API and no webpack build.

---
+++ Demo/Vue/StockTicker/wwwroot/app.js
@@ -0,0 +1,28 @@
+﻿const StockTicker = {
+  template: `
+    <form onsubmit="return false">
+      <input type="text" placeholder="Symbol " v-model="symbol" />
+      <button type="submit" @click="add">Add</button>
+      <ul v-for="(price, symbol) in StockPrices" :key="symbol">
+        <li>{{ symbol }}: {{ price }}</li>
+      </ul>
+    </form>
+`,
+  created() {
+    this.vm = dotnetify.vue.connect("StockTicker", this);
+  },
+  unmounted() {
+    this.vm.$destroy();
+  },
+  data() {
+    return { symbol: "", StockPrices: [] };
+  },
+  methods: {
+    add() {
+      this.vm.$dispatch({ AddSymbol: this.symbol });
+      this.symbol = "";
+    }
+  }
+};
+
+Vue.createApp(StockTicker).mount("#app");
