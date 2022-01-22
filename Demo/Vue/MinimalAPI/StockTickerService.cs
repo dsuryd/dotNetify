2253a445662d6b553f46ae604e362fd230ee52ed
dicky <dicky@DESKTOP-99P3RLO>
2022-01-21 18:09:54 -0800

    Minor refactor to use observable.

--- Demo/Vue/MinimalAPI/StockTickerService.cs
+++ Demo/Vue/MinimalAPI/StockTickerService.cs
@@ -1,32 +1,31 @@
 ﻿using System.Reactive.Subjects;
+using System.Reactive.Linq;
+using StockPriceDict = System.Collections.Generic.Dictionary<string, double>;
 
 namespace StockTicker;
 
 public interface IStockTickerService
 {
-   IObservable<Dictionary<string, double>> StockPrices { get; }
+   IObservable<StockPriceDict> StockPrices { get; }
 
    void AddSymbol(string symbol);
 }
 
 public class StockTickerService : IStockTickerService
 {
-   private readonly Random _random = new();
+   private readonly Subject<StockPriceDict> _stockPrices = new();
    private readonly List<string> _symbols = new();
-   private readonly Subject<Dictionary<string, double>> _stockPrices = new();
+   private readonly Random _random = new();
 
-   public IObservable<Dictionary<string, double>> StockPrices => _stockPrices;
+   public IObservable<StockPriceDict> StockPrices => _stockPrices;
 
    public StockTickerService()
    {
-      _ = new Timer(state =>
-        {
-           var prices = _symbols
-              .Select(x => KeyValuePair.Create(x, Math.Round(1000 * _random.NextDouble(), 2)))
-              .ToDictionary(x => x.Key, y => y.Value);
-
-           _stockPrices.OnNext(prices);
-        }, null, 0, 1000);
+      Observable.Interval(TimeSpan.FromSeconds(1))
+         .Select(_ => _symbols
+            .Select(x => KeyValuePair.Create(x, Math.Round(1000 * _random.NextDouble(), 2)))
+            .ToDictionary(x => x.Key, y => y.Value))
+         .Subscribe(_stockPrices);
    }
 
    public void AddSymbol(string symbol)
