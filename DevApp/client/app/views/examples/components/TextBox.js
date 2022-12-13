27b48940dc9a12c11eef14bad2654e911ad897be
dicky <dicky@DESKTOP-99P3RLO>
2022-12-12 16:24:55 -0800

    Port to react hook.

--- DevApp/client/app/views/examples/components/TextBox.js
+++ DevApp/client/app/views/examples/components/TextBox.js
@@ -1,51 +1,52 @@
-import React, { createRef } from "react";
+import React, { useRef, useLayoutEffect, useCallback } from "react";
 
-class TextBox extends React.Component {
-  constructor(props) {
-    super(props);
-    this.textInput = createRef();
-    this.state = { changed: false };
-  }
+const TextBox = ({ value, label, type, placeholder, errorText, onChange, onUpdate, onBlur }) => {
+  const textInput = useRef();
+  const changed = useRef(false);
 
-  focus() {
-    this.textInput.current.focus();
-  }
+  useLayoutEffect(() => {
+    textInput.current.focus();
+  }, []);
 
-  handleChange = event => {
-    this.setState({ changed: true });
-    this.props.onChange(event.target.value);
-  };
+  const handleChange = useCallback(
+    event => {
+      changed.current = true;
+      onChange && onChange(event.target.value);
+    },
+    [onChange]
+  );
 
-  handleBlur = event => {
-    if (this.state.changed && this.props.onUpdate) {
-      this.props.onUpdate(this.props.value);
-    }
-    this.setState({ changed: false });
-    this.props.onBlur && this.props.onBlur(event);
-  };
+  const handleBlur = useCallback(
+    event => {
+      if (changed.current && onUpdate) {
+        onUpdate(value);
+      }
+      changed.current = false;
+      onBlur && onBlur(event);
+    },
+    [onUpdate]
+  );
 
-  handleKeydown = event => {
-    if (event.keyCode == 13) this.handleBlur(event);
-  };
+  const handleKeydown = useCallback(event => {
+    if (event.keyCode == 13) handleBlur(event);
+  }, []);
 
-  render() {
-    return (
-      <div>
-        <label>{this.props.label}</label>
-        <input
-          type={this.props.type || "text"}
-          className="form-control"
-          value={this.props.value}
-          placeholder={this.props.placeholder}
-          onChange={this.handleChange}
-          onBlur={this.handleBlur}
-          onKeyDown={this.handleKeydown}
-          ref={this.textInput}
-        />
-        {this.props.errorText && <b>{this.props.errorText}</b>}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <label>{label}</label>
+      <input
+        type={type || "text"}
+        className="form-control"
+        value={value}
+        placeholder={placeholder}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        onKeyDown={handleKeydown}
+        ref={textInput}
+      />
+      {errorText && <b>{errorText}</b>}
+    </div>
+  );
+};
 
 export default TextBox;
