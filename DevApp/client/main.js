a53a983f935fd6a9196d2d1163ac0217dea666a2
dicky <dicky@DESKTOP-99P3RLO>
2022-12-12 18:13:31 -0800

    Override dotnetify react functions to use React 18 APIs.

--- DevApp/client/main.js
+++ DevApp/client/main.js
@@ -1,15 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot, hydrateRoot } from "react-dom/client";
 import dotnetify from "dotnetify";
 import App from "./app/views/App";
 import "./app/styles/app.css";
 import "./app/styles/prism.css";
 import * as views from "./app/views";
-
 //import { enableSsr } from 'dotnetify';
 //enableSsr();
 
 dotnetify.debug = true;
+
+// Override dotnetify functions that invoke older React APIs with React 18 APIs.
+dotnetify.react.router.render = (component, container) => {
+  const root = createRoot(container);
+  root.render(component);
+  return () => root.unmount();
+};
+dotnetify.react.router.hydrate = (component, container) => hydrateRoot(container, component);
 
 // Enable this to switch from JSON to MessagePack protocol
 /*
@@ -21,4 +27,10 @@ dotnetify.hubOptions.connectionBuilder = builder => builder.withHubProtocol(prot
 // Import all the routeable views into the global window variable.
 Object.assign(window, { ...views });
 
-ReactDOM.hydrate(<App />, document.getElementById("App"));
+const container = document.getElementById("App");
+hydrateRoot(container, <App />);
+
+// ** FOR DEBUGGING **
+//import HelloWorld from "./app/views/examples/react/HelloWorld";
+//createRoot(container).render(<HelloWorld />);
+// **
