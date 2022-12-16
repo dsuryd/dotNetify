139b432046e771b961954db82efa625b779cf4cc
dicky <dicky@DESKTOP-99P3RLO>
2022-12-16 11:58:06 -0800

    Implement http callback server.

--- DevApp/ws-server/index.js
+++ DevApp/ws-server/index.js
@@ -3,18 +3,25 @@
 const http = require("http"),
   axios = require("axios"),
   express = require("express"),
+  bodyParser = require("body-parser"),
   uuid = require("uuid"),
   WebSocket = require("ws");
 
-const serverPort = 3000;
-const server = http.createServer(express());
+const wsServer = http.createServer(express());
+const wsServerPort = 3000;
+
+const httpServer = express();
+const httpServerPort = 3010;
 
 const dotnetifyUrl = "http://localhost:5000/api/dotnetify/vm";
 
-const wss = new WebSocket.Server({ server });
+const wsClients = [];
+
+const wss = new WebSocket.Server({ server: wsServer });
 wss.on("connection", ws => {
   ws.id = uuid.v4();
   console.log(ws.id, "open");
+  wsClients.push(ws);
 
   const data = { connectionId: ws.id, state: "open" };
   axios.post(dotnetifyUrl, data).then(res => console.log("dotNetify", "open", res.status));
@@ -33,6 +40,7 @@ wss.on("connection", ws => {
 
   ws.on("close", () => {
     console.log(ws.id, "close");
+    wsClients = wsClients.filter(x => x !== ws);
   });
 
   ws.on("error", err => console.log(ws.id, "error", err));
@@ -40,6 +48,20 @@ wss.on("connection", ws => {
 
 wss.on("error", err => console.error("server error", err));
 
-server.listen(serverPort, () => {
-  console.log(`Websocket server started on port ` + serverPort);
+wsServer.listen(wsServerPort, () => {
+  console.log(`Websocket server started on port ` + wsServerPort);
+});
+
+// Connection callback.
+httpServer.use(bodyParser.json());
+httpServer.use(bodyParser.urlencoded({ extended: true }));
+httpServer.post("/:id", (req, res) => {
+  console.log(req.params.id, "callback", req.body);
+  const ws = wsClients.find(x => x.id === req.params.id);
+  if (ws) ws.send(JSON.stringify(req.body));
+  res.send("");
+});
+
+httpServer.listen(httpServerPort, () => {
+  console.log(`Http server started on port ` + httpServerPort);
 });
