edb7c068b4b43983416d4dbe15e53d87554fba88
dicky <dicky@DESKTOP-99P3RLO>
2022-12-21 15:59:51 -0800

    Cleanup.

--- DevApp/client/main.js
+++ DevApp/client/main.js
@@ -1,4 +1,4 @@
-import { hydrateRoot } from "react-dom/client";
+import { hydrateRoot, createRoot } from "react-dom/client";
 import dotnetify, { createWebSocketHub } from "dotnetify";
 import App from "./app/views/App";
 import "./app/styles/app.css";
@@ -6,22 +6,20 @@ import "./app/styles/prism.css";
 import * as views from "./app/views";
 import "../dist/react/v18-compatibility";
 
-// ** Uncomment this to enable SSR.
-//import { enableSsr } from 'dotnetify';
-//enableSsr();
-
 dotnetify.debug = true;
 
-// ** Uncomment this to enable integration with 3rd party websocket server like AWS API gateway **
-//dotnetify.hub = createWebSocketHub(process.env.AWS_WS_GATEWAY);
-//dotnetify.hub = createWebSocketHub("ws://localhost:3000");
+// ** Enable server-side rendering **
+// import { enableSsr } from 'dotnetify';
+// enableSsr();
 
-// ** Uncomment this to switch SignalR protocol from JSON to MessagePack **
-/*
-import { MessagePackHubProtocol } from '@microsoft/signalr-protocol-msgpack';
-const protocol = new MessagePackHubProtocol();
-dotnetify.hubOptions.connectionBuilder = builder => builder.withHubProtocol(protocol);
- */
+// ** Enable integration with 3rd party websocket server like AWS API gateway **
+//dotnetify.hub = createWebSocketHub(process.env.AWS_API_GATEWAY);
+//dotnetify.hub = createWebSocketHub(process.env.LOCAL_WS_SERVER);
+
+// ** Switch SignalR protocol from JSON to MessagePack **
+// import { MessagePackHubProtocol } from '@microsoft/signalr-protocol-msgpack';
+// const protocol = new MessagePackHubProtocol();
+// dotnetify.hubOptions.connectionBuilder = builder => builder.withHubProtocol(protocol);
 
 // Import all the routeable views into the global window variable.
 Object.assign(window, { ...views });
@@ -29,6 +27,6 @@ Object.assign(window, { ...views });
 const container = document.getElementById("App");
 hydrateRoot(container, <App />);
 
-// ** FOR DEV TESTING ONLY **
-// import TestApp from "./app/views/examples/react/SecurePage";
+// ** DEV TESTING **
+// import TestApp from "./app/views/examples/react/HelloWorld";
 // createRoot(container).render(<TestApp />);
